0dbd179a7b2733666e2ac6de26ac3a9d61c3b128
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-01-20 20:15:20 -0600

    fix fuzz tests

--- image_dds/fuzz/fuzz_targets/bcn_from_rgba8.rs
+++ image_dds/fuzz/fuzz_targets/bcn_from_rgba8.rs
@@ -2,10 +2,10 @@
 
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: (u32, u32, image_dds::CompressionFormat, &[u8])| {
+fuzz_target!(|input: (u32, u32, image_dds::ImageFormat, &[u8])| {
     let (width, height, format, data) = input;
     // TODO: Is it worth testing different quality settings?
     // Use fast for now sso each iteration is faster.
     let quality = image_dds::Quality::Fast;
-    let _bytes = image_dds::bcn::bcn_from_rgba8(width, height, data, format, quality);
+    let _bytes = image_dds::bcn::bcn_from_rgba8(width, height, data, format.into(), quality);
 });
--- image_dds/fuzz/fuzz_targets/rgba8_from_bcn.rs
+++ image_dds/fuzz/fuzz_targets/rgba8_from_bcn.rs
@@ -2,7 +2,7 @@
 
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: (u32, u32, image_dds::CompressionFormat, &[u8])| {
+fuzz_target!(|input: (u32, u32, image_dds::ImageFormat, &[u8])| {
     let (width, height, format, data) = input;
-    let _bytes = image_dds::bcn::rgba8_from_bcn(width, height, data, format);
+    let _bytes = image_dds::bcn::rgba8_from_bcn(width, height, data, format.into());
 });
