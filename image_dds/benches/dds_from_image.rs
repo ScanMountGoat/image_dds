b3066df694546014efc7e8045506153287d3dedf
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-11-08 08:44:14 -0600

    add criterion benchmarks

---
+++ image_dds/benches/dds_from_image.rs
@@ -0,0 +1,20 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use image::RgbaImage;
+use image_dds::{dds_from_image, ImageFormat, Mipmaps, Quality};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let image = RgbaImage::new(512, 512);
+    c.bench_function("dds_from_image", |b| {
+        b.iter(|| {
+            dds_from_image(
+                black_box(&image),
+                black_box(ImageFormat::BC7Unorm),
+                black_box(Quality::Fast),
+                black_box(Mipmaps::GeneratedAutomatic),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ image_dds/benches/image_from_dds.rs
@@ -0,0 +1,22 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use image_dds::{image_from_dds, ImageFormat, Surface};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    // Overestimate the surface size to avoid errors.
+    let surface = Surface {
+        width: 512,
+        height: 512,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: ImageFormat::BC7Unorm,
+        data: vec![0u8; 512 * 512 * 2],
+    };
+    let dds = surface.to_dds().unwrap();
+    c.bench_function("image_from_dds", |b| {
+        b.iter(|| image_from_dds(black_box(&dds), 0))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
