ee154c5faa4e42a5abba1b79a6c35f5236595596
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-01-27 12:34:24 -0600

    test zero filling in other formats

--- bcdec_rs/fuzz/fuzz_targets/bc4.rs
+++ bcdec_rs/fuzz/fuzz_targets/bc4.rs
@@ -6,12 +6,12 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: [u8; 16]| {
     // BC4 stores grayscale data, so each decompressed pixel is 1 byte.
-    let mut expected = [0u8; 4*4*1];
+    let mut expected = [255u8; 4*4*1];
     unsafe {
         bcndecode_sys::bcdec_bc4(data.as_ptr(), expected.as_mut_ptr() as _, 4*1);
     }
 
-    let mut actual = [0u8; 4*4*1];
+    let mut actual = [255u8; 4*4*1];
     bcdec_rs::bc4(&data, &mut actual, 4);
 
     assert_eq!(expected, actual);
