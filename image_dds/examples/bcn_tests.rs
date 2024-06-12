d28820ff70439ccca6ee389c15b5f689975a92ff
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-06-12 17:14:01 -0500

    generate bc2 and bc3 test data

--- image_dds/examples/bcn_tests.rs
+++ image_dds/examples/bcn_tests.rs
@@ -7,20 +7,47 @@ fn main() {
     // TODO: args to select the format.
     // Test data is based on the following blogpost:
     // https://fgiesen.wordpress.com/2021/10/04/gpu-bcn-decoding/
-    // TODO: Combine these into a single image?
-    let surface = bc1_r_test();
+    // BC1
+    let surface = bc1_r();
     let dds = surface.to_dds().unwrap();
     save_dds(dds, "bc1_r.dds");
 
-    let surface = bc1_g_test();
+    let surface = bc1_g();
     let dds = surface.to_dds().unwrap();
     save_dds(dds, "bc1_g.dds");
 
-    let surface = bc1_b_test();
+    let surface = bc1_b();
     let dds = surface.to_dds().unwrap();
     save_dds(dds, "bc1_b.dds");
 
-    // TODO: BC2, BC3, BC4, BC5
+    // BC2
+    let surface = bc2_r();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc2_r.dds");
+
+    let surface = bc2_g();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc2_g.dds");
+
+    let surface = bc2_b();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc2_b.dds");
+
+    // BC3
+    let surface = bc3_r();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc3_r.dds");
+
+    let surface = bc3_g();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc3_g.dds");
+
+    let surface = bc3_b();
+    let dds = surface.to_dds().unwrap();
+    save_dds(dds, "bc3_b.dds");
+
+    // TODO: BC4, BC5
+
     // TODO: How to handle BC6 and BC7?
 }
 
@@ -29,7 +56,8 @@ fn save_dds(dds: ddsfile::Dds, path: &str) {
     dds.write(&mut writer).unwrap();
 }
 
-fn bc1_r_test() -> Surface<Vec<u8>> {
+// TODO: reduce repetition
+fn bc1_r() -> Surface<Vec<u8>> {
     // 5 bit independent R channel for BC1 end points.
     let mut data = Vec::new();
     for i in 0..32 {
@@ -49,12 +77,12 @@ fn bc1_r_test() -> Surface<Vec<u8>> {
     }
 }
 
-fn bc1_b_test() -> Surface<Vec<u8>> {
+fn bc1_b() -> Surface<Vec<u8>> {
     // 5 bit independent B channel for BC1 end points.
     let mut data = Vec::new();
     for i in 0..32 {
         for j in 0..32 {
-            let block = bc1_block(0, 0, j, 0, 0, i);
+            let block = bc1_block(0, 0, i, 0, 0, j);
             data.extend_from_slice(&block.to_le_bytes());
         }
     }
@@ -69,7 +97,7 @@ fn bc1_b_test() -> Surface<Vec<u8>> {
     }
 }
 
-fn bc1_g_test() -> Surface<Vec<u8>> {
+fn bc1_g() -> Surface<Vec<u8>> {
     let mut data = Vec::new();
     // 6 bit independent G channel for BC1 end points.
     for i in 0..64 {
@@ -94,8 +122,158 @@ fn bc1_block(r0: u64, g0: u64, b0: u64, r1: u64, g1: u64, b1: u64) -> u64 {
     let c1 = (r1 << 11) | (g1 << 5) | b1;
 
     // Use each unique 2 bit value for the 4x4 indices.
-    let indices = 0b00011011000110110001101100011011;
+    let indices = bit_indices(4 * 4, 2);
 
-    let block: u64 = (indices << 32) | (c1 << 16) | c0;
-    block
+    (indices << 32) | (c1 << 16) | c0
+}
+
+fn bc2_r() -> Surface<Vec<u8>> {
+    // 5 bit independent R channel for BC2 end points.
+    let mut data = Vec::new();
+    for i in 0..32 {
+        for j in 0..32 {
+            let block = bc2_block(i, 0, 0, j, 0, 0);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 128,
+        height: 128,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC2RgbaUnorm,
+        data,
+    }
+}
+
+fn bc2_b() -> Surface<Vec<u8>> {
+    // 5 bit independent B channel for BC2 end points.
+    let mut data = Vec::new();
+    for i in 0..32 {
+        for j in 0..32 {
+            let block = bc2_block(0, 0, j, 0, 0, i);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 128,
+        height: 128,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC2RgbaUnorm,
+        data,
+    }
+}
+
+fn bc2_g() -> Surface<Vec<u8>> {
+    let mut data = Vec::new();
+    // 6 bit independent G channel for BC2 end points.
+    for i in 0..64 {
+        for j in 0..64 {
+            let block = bc2_block(0, i, 0, 0, j, 0);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 256,
+        height: 256,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC2RgbaUnorm,
+        data,
+    }
+}
+
+fn bc2_block(r0: u64, g0: u64, b0: u64, r1: u64, g1: u64, b1: u64) -> u128 {
+    // Generate each unique 4 bit alpha value.
+    // These conveniently fit in a single block.
+    let alpha_block = bit_indices(4 * 4, 4);
+
+    // BC2 combines a BC1 RGB block with a separate alpha block.
+    ((bc1_block(r0, g0, b0, r1, g1, b1) as u128) << 64) | alpha_block as u128
+}
+
+fn bc3_r() -> Surface<Vec<u8>> {
+    // 5 bit independent R channel for BC3 end points.
+    // 8 bit alpha end points require more pixels.
+    let mut data = Vec::new();
+    for i in 0..256 {
+        for j in 0..256 {
+            let block = bc3_block(i % 32, 0, 0, i, j % 32, 0, 0, j);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 1024,
+        height: 1024,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC3RgbaUnorm,
+        data,
+    }
+}
+
+fn bc3_b() -> Surface<Vec<u8>> {
+    // 5 bit independent B channel for BC3 end points.
+    // 8 bit alpha end points require more pixels.
+    let mut data = Vec::new();
+    for i in 0..256 {
+        for j in 0..256 {
+            let block = bc3_block(0, 0, i % 32, i, 0, 0, j % 32, j);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 1024,
+        height: 1024,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC3RgbaUnorm,
+        data,
+    }
+}
+
+fn bc3_g() -> Surface<Vec<u8>> {
+    let mut data = Vec::new();
+    // 6 bit G and independent G channel for BC3 end points.
+    // 8 bit alpha end points require more pixels.
+    for i in 0..256 {
+        for j in 0..256 {
+            let block = bc3_block(0, i % 64, 0, i, 0, j % 64, 0, j);
+            data.extend_from_slice(&block.to_le_bytes());
+        }
+    }
+    Surface {
+        width: 1024,
+        height: 1024,
+        depth: 1,
+        layers: 1,
+        mipmaps: 1,
+        image_format: image_dds::ImageFormat::BC3RgbaUnorm,
+        data,
+    }
+}
+
+fn bc3_block(r0: u64, g0: u64, b0: u64, a0: u64, r1: u64, g1: u64, b1: u64, a1: u64) -> u128 {
+    // Use each unique 3 bit value for the 4x4 indices.
+    let indices = bit_indices(4 * 4, 3);
+
+    let alpha_block = (indices << 16) | (a1 << 8) | a0;
+
+    // BC3 combines a BC1 RGB block with a separate alpha block.
+    ((bc1_block(r0, g0, b0, r1, g1, b1) as u128) << 64) | alpha_block as u128
+}
+
+fn bit_indices(count: u64, bits: u64) -> u64 {
+    // Repeat unique bit patterns for count.
+    let mut indices = 0;
+    for i in 0..count {
+        indices |= (i % (1 << bits)) << (i * bits);
+    }
+    indices
 }
