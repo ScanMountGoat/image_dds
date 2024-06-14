5aebef40aed281fd93d1b18284412275e0760fc9
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-06-14 18:14:12 -0500

    move bcn test images to bcn_test crate

--- bcn_test/src/main.rs
+++ bcn_test/src/main.rs
@@ -63,7 +63,7 @@ fn main() {
     // TODO: How to handle BC6 and BC7?
 }
 
-fn save_dds(dds: ddsfile::Dds, path: &str) {
+fn save_dds(dds: image_dds::ddsfile::Dds, path: &str) {
     let mut writer = BufWriter::new(std::fs::File::create(path).unwrap());
     dds.write(&mut writer).unwrap();
 }
@@ -133,7 +133,7 @@ fn bc3_r() -> Surface<Vec<u8>> {
     // 5-bit independent R channel for BC3 end points.
     // 8-bit alpha end points require more pixels.
     bcn(8, image_dds::ImageFormat::BC3RgbaUnorm, |i, j| {
-        bc3_block(i % 32, 0, 0, i, j % 32, 0, 0, j).to_le_bytes()
+        bc3_block((i % 32, 0, 0, i), (j % 32, 0, 0, j)).to_le_bytes()
     })
 }
 
@@ -141,7 +141,7 @@ fn bc3_b() -> Surface<Vec<u8>> {
     // 5-bit independent B channel for BC3 end points.
     // 8-bit alpha end points require more pixels.
     bcn(8, image_dds::ImageFormat::BC3RgbaUnorm, |i, j| {
-        bc3_block(0, 0, i % 32, i, 0, 0, j % 32, j).to_le_bytes()
+        bc3_block((0, 0, i % 32, i), (0, 0, j % 32, j)).to_le_bytes()
     })
 }
 
@@ -149,11 +149,14 @@ fn bc3_g() -> Surface<Vec<u8>> {
     // 6-bit G and independent G channel for BC3 end points.
     // 8-bit alpha end points require more pixels.
     bcn(8, image_dds::ImageFormat::BC3RgbaUnorm, |i, j| {
-        bc3_block(0, i % 64, 0, i, 0, j % 64, 0, j).to_le_bytes()
+        bc3_block((0, i % 64, 0, i), (0, j % 64, 0, j)).to_le_bytes()
     })
 }
 
-fn bc3_block(r0: u64, g0: u64, b0: u64, a0: u64, r1: u64, g1: u64, b1: u64, a1: u64) -> u128 {
+fn bc3_block(rgba0: (u64, u64, u64, u64), rgba1: (u64, u64, u64, u64)) -> u128 {
+    let (r0, g0, b0, a0) = rgba0;
+    let (r1, g1, b1, a1) = rgba1;
+
     let alpha_block = smooth_alpha_block(a0, a1);
 
     // BC3 combines a BC1 RGB block with a separate alpha block.
@@ -164,8 +167,7 @@ fn smooth_alpha_block(a0: u64, a1: u64) -> u64 {
     // Use each unique 3-bit value for the 4x4 indices.
     let indices = bit_indices(4 * 4, 3);
 
-    let alpha_block = (indices << 16) | (a1 << 8) | a0;
-    alpha_block
+    (indices << 16) | (a1 << 8) | a0
 }
 
 fn bc4_r() -> Surface<Vec<u8>> {
@@ -241,4 +243,85 @@ fn bit_indices(count: u64, bits: u64) -> u64 {
         indices |= (i % (1 << bits)) << (i * bits);
     }
     indices
+}
+
+#[cfg(test)]
+mod tests {
+    fn test_dds_decode(dds_bytes: &[u8], image_bytes: &[u8]) {
+        // Test the entire dds -> decode -> image pipeline.
+        let mut reader = std::io::Cursor::new(dds_bytes);
+        let dds = image_dds::ddsfile::Dds::read(&mut reader).unwrap();
+
+        let expected = image::load_from_memory(image_bytes).unwrap().to_rgba8();
+        let actual = image_dds::image_from_dds(&dds, 0).unwrap();
+        assert!(expected == actual);
+    }
+
+    #[test]
+    fn image_from_dds_bc1() {
+        test_dds_decode(
+            include_bytes!("../data/bc1_r.dds"),
+            include_bytes!("../data/bc1_r.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc1_g.dds"),
+            include_bytes!("../data/bc1_g.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc1_b.dds"),
+            include_bytes!("../data/bc1_b.png"),
+        );
+    }
+
+    #[test]
+    fn image_from_dds_bc2() {
+        test_dds_decode(
+            include_bytes!("../data/bc2_r.dds"),
+            include_bytes!("../data/bc2_r.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc2_g.dds"),
+            include_bytes!("../data/bc2_g.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc2_b.dds"),
+            include_bytes!("../data/bc2_b.png"),
+        );
+    }
+
+    #[test]
+    fn image_from_dds_bc3() {
+        test_dds_decode(
+            include_bytes!("../data/bc3_r.dds"),
+            include_bytes!("../data/bc3_r.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc3_g.dds"),
+            include_bytes!("../data/bc3_g.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc3_b.dds"),
+            include_bytes!("../data/bc3_b.png"),
+        );
+    }
+
+    #[test]
+    fn image_from_dds_bc4() {
+        test_dds_decode(
+            include_bytes!("../data/bc4_r.dds"),
+            include_bytes!("../data/bc4_r.png"),
+        );
+    }
+
+    #[test]
+    fn image_from_dds_bc5() {
+        test_dds_decode(
+            include_bytes!("../data/bc5_r.dds"),
+            include_bytes!("../data/bc5_r.png"),
+        );
+        test_dds_decode(
+            include_bytes!("../data/bc5_g.dds"),
+            include_bytes!("../data/bc5_g.png"),
+        );
+    }
 }
