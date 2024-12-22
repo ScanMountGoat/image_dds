700dbd6fdbf8f97f428b0d7ff7be0ccae12bfc00
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-12-22 16:05:10 -0600

    fix compile error

--- bcndecode-sys/src/lib.rs
+++ bcndecode-sys/src/lib.rs
@@ -1,58 +1,58 @@
 use std::ffi::c_int;
 
-unsafe extern "C" {
-    pub unsafe fn bcdec_bc1(
+extern "C" {
+    pub fn bcdec_bc1(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
     );
-    pub unsafe fn bcdec_bc2(
+    pub fn bcdec_bc2(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
     );
-    pub unsafe fn bcdec_bc3(
+    pub fn bcdec_bc3(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
     );
-    pub unsafe fn bcdec_bc4(
-        compressed_block: *const u8,
-        decompressed_block: *mut u8,
-        destination_pitch: c_int,
-        is_signed: c_int,
-    );
-    pub unsafe fn bcdec_bc5(
+    pub fn bcdec_bc4(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
         is_signed: c_int,
     );
-    pub unsafe fn bcdec_bc4_float(
+    pub fn bcdec_bc5(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
         is_signed: c_int,
     );
-    pub unsafe fn bcdec_bc5_float(
+    pub fn bcdec_bc4_float(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
         is_signed: c_int,
     );
-    pub unsafe fn bcdec_bc6h_float(
+    pub fn bcdec_bc5_float(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
         is_signed: c_int,
     );
-    pub unsafe fn bcdec_bc6h_half(
+    pub fn bcdec_bc6h_float(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
         is_signed: c_int,
     );
-    pub unsafe fn bcdec_bc7(
+    pub fn bcdec_bc6h_half(
+        compressed_block: *const u8,
+        decompressed_block: *mut u8,
+        destination_pitch: c_int,
+        is_signed: c_int,
+    );
+    pub fn bcdec_bc7(
         compressed_block: *const u8,
         decompressed_block: *mut u8,
         destination_pitch: c_int,
