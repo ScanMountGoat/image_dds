790d88cf6263af65deff8f3fb585358bf74109b6
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2025-01-06 19:01:59 -0600

    add reference functions for snorm conversions

--- image_dds/src/lib.rs
+++ image_dds/src/lib.rs
@@ -398,7 +398,6 @@ fn mip_size(
 
 fn snorm_to_unorm(x: u8) -> u8 {
     // Validated against decoding R8Snorm DDS with GPU and paint.net (DirectXTex).
-    // TODO: Is this the optimal way to write this?
     if x < 128 {
         x + 128
     } else if x == 128 {
@@ -410,7 +409,6 @@ fn snorm_to_unorm(x: u8) -> u8 {
 
 fn unorm_to_snorm(x: u8) -> u8 {
     // Inverse of snorm_to_unorm.
-    // TODO: Is this the optimal way to write this?
     if x >= 128 {
         x - 128
     } else if x == 127 {
@@ -590,26 +588,29 @@ mod tests {
         );
     }
 
+    fn snorm_to_unorm_reference(x: i8) -> u8 {
+        // Remap [-1, 1] to [0, 1] to fit in an unsigned integer.
+        (((x as f32 / 127.0).max(-1.0) * 0.5 + 0.5) * 255.0).round() as u8
+    }
+
+    fn unorm_to_snorm_reference(x: u8) -> i8 {
+        // Remap [0, 1] to [-1, 1] and convert to float.
+        (((x as f32 / 255.0) * 2.0 - 1.0) * 127.0).round() as i8
+    }
+
     #[test]
     fn convert_snorm_to_unorm() {
-        let expected = [
-            128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
-            145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161,
-            162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178,
-            179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195,
-            196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212,
-            213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229,
-            230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246,
-            247, 248, 249, 250, 251, 252, 253, 254, 255, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-            34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
-            56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
-            78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
-            100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
-            117, 118, 119, 120, 121, 122, 123, 124, 125, 126,
-        ];
-        for (input, output) in expected.into_iter().enumerate() {
-            assert_eq!(snorm_to_unorm(input as u8), output);
+        // 128, ..., 255, 0, ..., 126
+        for i in 0..=255 {
+            assert_eq!(snorm_to_unorm(i), snorm_to_unorm_reference(i as i8));
+        }
+    }
+
+    #[test]
+    fn convert_unorm_to_snorm() {
+        // 129, ..., 255, 0, ..., 127
+        for i in 0..=255 {
+            assert_eq!(unorm_to_snorm(i) as i8, unorm_to_snorm_reference(i));
         }
     }
 
