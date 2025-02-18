f36657a2d5694b09a3702b913cf5ab75b66bcb29
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2025-02-17 19:00:55 -0600

    clean up rgba conversions with generics

--- image_dds/src/rgba.rs
+++ image_dds/src/rgba.rs
@@ -7,53 +7,29 @@ use crate::{
     unorm8_to_unorm16, unorm8_to_unorm4, SurfaceError,
 };
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgba8([u8; 4]);
+pub type R8 = R<u8>;
+pub type Rg8 = Rg<u8>;
+pub type Rgb8 = Rgb<u8>;
+pub type Rgba8 = Rgba<u8>;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rf16([f16; 1]);
+pub type R8Snorm = R<i8>;
+pub type Rg8Snorm = Rg<i8>;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgf16([f16; 2]);
+pub type Rf16 = R<f16>;
+pub type Rgf16 = Rg<f16>;
+pub type Rgbaf16 = Rgba<f16>;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgbaf16([f16; 4]);
+pub type Rf32 = R<f32>;
+pub type Rgf32 = Rg<f32>;
+pub type Rgbaf32 = Rgba<f32>;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rf32([f32; 1]);
+pub type R16 = R<u16>;
+pub type Rg16 = Rg<u16>;
+pub type Rgba16 = Rgba<u16>;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgf32([f32; 2]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgbaf32([f32; 4]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct R8([u8; 1]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct R8Snorm([u8; 1]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rg8([u8; 2]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rg8Snorm([u8; 2]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgb8([u8; 3]);
+pub type R16Snorm = R<i16>;
+pub type Rg16Snorm = Rg<i16>;
+pub type Rgba16Snorm = Rgba<i16>;
 
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable)]
@@ -67,38 +43,14 @@ pub struct Bgra8([u8; 4]);
 #[derive(Clone, Copy, Pod, Zeroable)]
 pub struct Bgra4([u8; 2]);
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct R16([u16; 1]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct R16Snorm([u16; 1]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rg16([u16; 2]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rg16Snorm([u16; 2]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgba16([u16; 4]);
-
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct Rgba16Snorm([u16; 4]);
-
-pub trait Pixel {
+pub trait GetPixel {
     fn get_pixel(data: &[u8], index: usize) -> Self;
 }
 
 macro_rules! pixel_impl {
     ($($ty:ty),*) => {
         $(
-            impl Pixel for $ty {
+            impl GetPixel for $ty {
                 fn get_pixel(data: &[u8], index: usize) -> Self {
                     Self(pixel_from_bytes(data, index))
                 }
@@ -106,6 +58,7 @@ macro_rules! pixel_impl {
         )*
     };
 }
+// TODO: Implement automatically for generic types?
 pixel_impl!(
     R8,
     R8Snorm,
@@ -130,8 +83,6 @@ pixel_impl!(
     Rgbaf32
 );
 
-// TODO: Implement using macro or generic function?
-// num channels, channel swizzles, function or value for each channel conversion?
 pub trait ToRgba<T> {
     fn to_rgba(self) -> [T; 4];
 }
@@ -150,6 +101,12 @@ impl FromBytes for u8 {
     }
 }
 
+impl FromBytes for i8 {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        bytes[0] as i8
+    }
+}
+
 macro_rules! from_bytes_impl {
     ($($ty:ty),*) => {
         $(
@@ -162,7 +119,280 @@ macro_rules! from_bytes_impl {
         )*
     };
 }
-from_bytes_impl!(u16, f16, f32);
+from_bytes_impl!(u16, i16, f16, f32);
+
+// TODO: Create a module for pixel conversions?
+trait Channel {
+    fn to_unorm8(self) -> u8;
+    fn from_unorm8(u: u8) -> Self;
+    fn to_f32(self) -> f32;
+    fn from_f32(f: f32) -> Self;
+    const ZERO: Self;
+}
+
+impl Channel for u8 {
+    const ZERO: Self = 0;
+
+    fn to_unorm8(self) -> u8 {
+        self
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        u
+    }
+
+    fn to_f32(self) -> f32 {
+        self as f32 / 255.0
+    }
+
+    fn from_f32(f: f32) -> Self {
+        (f * 255.0) as u8
+    }
+}
+
+impl Channel for i8 {
+    const ZERO: Self = 0;
+
+    fn to_unorm8(self) -> u8 {
+        snorm8_to_unorm8(self as u8)
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        unorm8_to_snorm8(u) as i8
+    }
+
+    fn to_f32(self) -> f32 {
+        snorm8_to_float(self as u8)
+    }
+
+    fn from_f32(f: f32) -> Self {
+        float_to_snorm8(f)
+    }
+}
+
+impl Channel for u16 {
+    const ZERO: Self = 0;
+
+    fn to_unorm8(self) -> u8 {
+        unorm16_to_unorm8(self)
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        unorm8_to_unorm16(u)
+    }
+
+    fn to_f32(self) -> f32 {
+        self as f32 / 65535.0
+    }
+
+    fn from_f32(f: f32) -> Self {
+        (f * 65535.0) as u16
+    }
+}
+
+impl Channel for i16 {
+    const ZERO: Self = 0;
+
+    fn to_unorm8(self) -> u8 {
+        snorm16_to_unorm8(self as u16)
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        unorm8_to_snorm16(u)
+    }
+
+    fn to_f32(self) -> f32 {
+        snorm16_to_float(self as u16)
+    }
+
+    fn from_f32(f: f32) -> Self {
+        float_to_snorm16(f)
+    }
+}
+
+impl Channel for f16 {
+    const ZERO: Self = f16::ZERO;
+
+    fn to_unorm8(self) -> u8 {
+        (self.to_f32() * 255.0) as u8
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        f16::from_f32(u as f32 / 255.0)
+    }
+
+    fn to_f32(self) -> f32 {
+        self.to_f32()
+    }
+
+    fn from_f32(f: f32) -> Self {
+        f16::from_f32(f)
+    }
+}
+
+impl Channel for f32 {
+    const ZERO: Self = 0.0;
+
+    fn to_unorm8(self) -> u8 {
+        (self * 255.0) as u8
+    }
+
+    fn from_unorm8(u: u8) -> Self {
+        u as f32 / 255.0
+    }
+
+    fn to_f32(self) -> f32 {
+        self
+    }
+
+    fn from_f32(f: f32) -> Self {
+        f
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct R<T>([T; 1]);
+
+impl<T: Channel> FromRgba<u8> for R<T> {
+    fn from_rgba(rgba: [u8; 4]) -> Self {
+        Self([T::from_unorm8(rgba[0])])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<u8> for R<T> {
+    fn to_rgba(self) -> [u8; 4] {
+        let r = T::to_unorm8(self.0[0]);
+        [r, r, r, 255u8]
+    }
+}
+
+impl<T: Channel> FromRgba<f32> for R<T> {
+    fn from_rgba(rgba: [f32; 4]) -> Self {
+        Self([T::from_f32(rgba[0])])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<f32> for R<T> {
+    fn to_rgba(self) -> [f32; 4] {
+        let r = T::to_f32(self.0[0]);
+        [r, r, r, 1.0]
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct Rg<T>([T; 2]);
+
+impl<T: Channel> FromRgba<u8> for Rg<T> {
+    fn from_rgba(rgba: [u8; 4]) -> Self {
+        Self([T::from_unorm8(rgba[0]), T::from_unorm8(rgba[1])])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<u8> for Rg<T> {
+    fn to_rgba(self) -> [u8; 4] {
+        // The blue channel converts 0 for unorm/snorm by convention.
+        [
+            T::to_unorm8(self.0[0]),
+            T::to_unorm8(self.0[1]),
+            T::to_unorm8(T::ZERO),
+            255u8,
+        ]
+    }
+}
+
+impl<T: Channel> FromRgba<f32> for Rg<T> {
+    fn from_rgba(rgba: [f32; 4]) -> Self {
+        Self([T::from_f32(rgba[0]), T::from_f32(rgba[1])])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<f32> for Rg<T> {
+    fn to_rgba(self) -> [f32; 4] {
+        // The blue channel converts 0 for unorm/snorm by convention.
+        [
+            T::to_f32(self.0[0]),
+            T::to_f32(self.0[1]),
+            T::to_f32(T::ZERO),
+            1.0,
+        ]
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct Rgb<T>([T; 3]);
+
+impl<T: Channel> FromRgba<u8> for Rgb<T> {
+    fn from_rgba(rgba: [u8; 4]) -> Self {
+        Self([
+            T::from_unorm8(rgba[0]),
+            T::from_unorm8(rgba[1]),
+            T::from_unorm8(rgba[2]),
+        ])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<u8> for Rgb<T> {
+    fn to_rgba(self) -> [u8; 4] {
+        [
+            T::to_unorm8(self.0[0]),
+            T::to_unorm8(self.0[1]),
+            T::to_unorm8(self.0[2]),
+            255u8,
+        ]
+    }
+}
+
+impl<T: Channel> FromRgba<f32> for Rgb<T> {
+    fn from_rgba(rgba: [f32; 4]) -> Self {
+        Self([
+            T::from_f32(rgba[0]),
+            T::from_f32(rgba[1]),
+            T::from_f32(rgba[2]),
+        ])
+    }
+}
+
+impl<T: Channel + Copy> ToRgba<f32> for Rgb<T> {
+    fn to_rgba(self) -> [f32; 4] {
+        [
+            T::to_f32(self.0[0]),
+            T::to_f32(self.0[1]),
+            T::to_f32(self.0[2]),
+            1.0,
+        ]
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct Rgba<T>([T; 4]);
+
+impl<T: Channel> FromRgba<u8> for Rgba<T> {
+    fn from_rgba(rgba: [u8; 4]) -> Self {
+        Self(rgba.map(T::from_unorm8))
+    }
+}
+
+impl<T: Channel> ToRgba<u8> for Rgba<T> {
+    fn to_rgba(self) -> [u8; 4] {
+        self.0.map(T::to_unorm8)
+    }
+}
+
+impl<T: Channel> FromRgba<f32> for Rgba<T> {
+    fn from_rgba(rgba: [f32; 4]) -> Self {
+        Self(rgba.map(T::from_f32))
+    }
+}
+
+impl<T: Channel> ToRgba<f32> for Rgba<T> {
+    fn to_rgba(self) -> [f32; 4] {
+        self.0.map(T::to_f32)
+    }
+}
 
 fn pixel_from_bytes<const N: usize, P: FromBytes>(data: &[u8], index: usize) -> [P; N] {
     std::array::from_fn(|i| {
@@ -174,277 +404,6 @@ fn pixel_from_bytes<const N: usize, P: FromBytes>(data: &[u8], index: usize) -> 
 
 fn get_pixel<T>(data: &[T], index: usize, size: usize) -> &[T] {
     &data[index * size..index * size + size]
-}
-
-impl ToRgba<u8> for Rgba8 {
-    fn to_rgba(self) -> [u8; 4] {
-        self.0
-    }
-}
-
-impl FromRgba<u8> for Rgba8 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self(rgba)
-    }
-}
-
-impl ToRgba<u8> for Rgbaf16 {
-    fn to_rgba(self) -> [u8; 4] {
-        self.0.map(|f| (f.to_f32() * 255.0) as u8)
-    }
-}
-
-impl FromRgba<u8> for Rgf16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([
-            f16::from_f32(rgba[0] as f32 / 255.0),
-            f16::from_f32(rgba[1] as f32 / 255.0),
-        ])
-    }
-}
-
-impl ToRgba<f32> for Rgf16 {
-    fn to_rgba(self) -> [f32; 4] {
-        [self.0[0].to_f32(), self.0[1].to_f32(), 0.0, 1.0]
-    }
-}
-
-impl FromRgba<f32> for Rgf16 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([f16::from_f32(rgba[0]), f16::from_f32(rgba[1])])
-    }
-}
-
-impl ToRgba<u8> for Rgf16 {
-    fn to_rgba(self) -> [u8; 4] {
-        [
-            (self.0[0].to_f32() * 255.0) as u8,
-            (self.0[1].to_f32() * 255.0) as u8,
-            0u8,
-            255u8,
-        ]
-    }
-}
-
-impl FromRgba<u8> for Rgbaf16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self(rgba.map(|u| f16::from_f32(u as f32 / 255.0)))
-    }
-}
-
-impl ToRgba<f32> for Rgbaf16 {
-    fn to_rgba(self) -> [f32; 4] {
-        self.0.map(f16::to_f32)
-    }
-}
-
-impl FromRgba<f32> for Rgbaf16 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self(rgba.map(f16::from_f32))
-    }
-}
-
-impl FromRgba<u8> for Rgf32 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([rgba[0] as f32 / 255.0, rgba[1] as f32 / 255.0])
-    }
-}
-
-impl ToRgba<f32> for Rgf32 {
-    fn to_rgba(self) -> [f32; 4] {
-        [self.0[0], self.0[1], 0.0, 1.0]
-    }
-}
-
-impl FromRgba<f32> for Rgf32 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([rgba[0], rgba[1]])
-    }
-}
-
-impl ToRgba<u8> for Rgf32 {
-    fn to_rgba(self) -> [u8; 4] {
-        [
-            (self.0[0] * 255.0) as u8,
-            (self.0[1] * 255.0) as u8,
-            0u8,
-            255u8,
-        ]
-    }
-}
-
-impl ToRgba<u8> for Rgbaf32 {
-    fn to_rgba(self) -> [u8; 4] {
-        self.0.map(|f| (f * 255.0) as u8)
-    }
-}
-
-impl FromRgba<u8> for Rgbaf32 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self(rgba.map(|u| u as f32 / 255.0))
-    }
-}
-
-impl ToRgba<f32> for Rgbaf32 {
-    fn to_rgba(self) -> [f32; 4] {
-        self.0
-    }
-}
-
-impl FromRgba<f32> for Rgbaf32 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self(rgba)
-    }
-}
-
-impl ToRgba<u8> for Rf32 {
-    fn to_rgba(self) -> [u8; 4] {
-        let r = (self.0[0] * 255.0) as u8;
-        [r, r, r, 255u8]
-    }
-}
-
-impl FromRgba<u8> for Rf32 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([rgba[0] as f32 / 255.0])
-    }
-}
-
-impl ToRgba<f32> for Rf32 {
-    fn to_rgba(self) -> [f32; 4] {
-        [self.0[0], self.0[0], self.0[0], 1.0]
-    }
-}
-
-impl FromRgba<f32> for Rf32 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([rgba[0]])
-    }
-}
-
-impl ToRgba<u8> for Rf16 {
-    fn to_rgba(self) -> [u8; 4] {
-        let r = (self.0[0].to_f32() * 255.0) as u8;
-        [r, r, r, 255u8]
-    }
-}
-
-impl FromRgba<u8> for Rf16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([f16::from_f32(rgba[0] as f32 / 255.0)])
-    }
-}
-
-impl ToRgba<f32> for Rf16 {
-    fn to_rgba(self) -> [f32; 4] {
-        let r = self.0[0].to_f32();
-        [r, r, r, 1.0]
-    }
-}
-
-impl FromRgba<f32> for Rf16 {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([f16::from_f32(rgba[0])])
-    }
-}
-
-impl ToRgba<u8> for R8 {
-    fn to_rgba(self) -> [u8; 4] {
-        [self.0[0], self.0[0], self.0[0], 255u8]
-    }
-}
-
-impl FromRgba<u8> for R8 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([rgba[0]])
-    }
-}
-
-impl ToRgba<u8> for R8Snorm {
-    fn to_rgba(self) -> [u8; 4] {
-        let r = snorm8_to_unorm8(self.0[0]);
-        [r, r, r, 255u8]
-    }
-}
-
-impl FromRgba<u8> for R8Snorm {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([unorm8_to_snorm8(rgba[0])])
-    }
-}
-
-impl ToRgba<f32> for R8Snorm {
-    fn to_rgba(self) -> [f32; 4] {
-        let r = snorm8_to_float(self.0[0]);
-        [r, r, r, 1.0]
-    }
-}
-
-impl FromRgba<f32> for R8Snorm {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([float_to_snorm8(rgba[0]) as u8])
-    }
-}
-
-impl ToRgba<u8> for Rg8 {
-    fn to_rgba(self) -> [u8; 4] {
-        [self.0[0], self.0[1], 0u8, 255u8]
-    }
-}
-
-impl FromRgba<u8> for Rg8 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([rgba[0], rgba[1]])
-    }
-}
-
-impl ToRgba<u8> for Rg8Snorm {
-    fn to_rgba(self) -> [u8; 4] {
-        [
-            snorm8_to_unorm8(self.0[0]),
-            snorm8_to_unorm8(self.0[1]),
-            snorm8_to_unorm8(0u8),
-            255u8,
-        ]
-    }
-}
-
-impl FromRgba<u8> for Rg8Snorm {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([unorm8_to_snorm8(rgba[0]), unorm8_to_snorm8(rgba[1])])
-    }
-}
-
-impl ToRgba<f32> for Rg8Snorm {
-    fn to_rgba(self) -> [f32; 4] {
-        [
-            snorm8_to_float(self.0[0]),
-            snorm8_to_float(self.0[1]),
-            snorm8_to_float(0u8),
-            1.0,
-        ]
-    }
-}
-
-impl FromRgba<f32> for Rg8Snorm {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([
-            float_to_snorm8(rgba[0]) as u8,
-            float_to_snorm8(rgba[1]) as u8,
-        ])
-    }
-}
-
-impl ToRgba<u8> for Rgb8 {
-    fn to_rgba(self) -> [u8; 4] {
-        [self.0[0], self.0[1], self.0[2], 255u8]
-    }
-}
-
-impl FromRgba<u8> for Rgb8 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([rgba[0], rgba[1], rgba[2]])
-    }
 }
 
 impl ToRgba<u8> for Bgr8 {
@@ -495,138 +454,6 @@ impl FromRgba<u8> for Bgra4 {
     }
 }
 
-impl ToRgba<u8> for R16 {
-    fn to_rgba(self) -> [u8; 4] {
-        let r = unorm16_to_unorm8(self.0[0]);
-        [r, r, r, 255u8]
-    }
-}
-
-impl FromRgba<u8> for R16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([unorm8_to_unorm16(rgba[0])])
-    }
-}
-
-impl ToRgba<u8> for R16Snorm {
-    fn to_rgba(self) -> [u8; 4] {
-        let r = snorm16_to_unorm8(self.0[0]);
-        [r, r, r, 255u8]
-    }
-}
-
-impl FromRgba<u8> for R16Snorm {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([unorm8_to_snorm16(rgba[0]) as u16])
-    }
-}
-
-impl ToRgba<f32> for R16Snorm {
-    fn to_rgba(self) -> [f32; 4] {
-        let r = snorm16_to_float(self.0[0]);
-        [r, r, r, 1.0]
-    }
-}
-
-impl FromRgba<f32> for R16Snorm {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([float_to_snorm16(rgba[0]) as u16])
-    }
-}
-
-impl ToRgba<u8> for Rg16 {
-    fn to_rgba(self) -> [u8; 4] {
-        [
-            unorm16_to_unorm8(self.0[0]),
-            unorm16_to_unorm8(self.0[1]),
-            0u8,
-            255u8,
-        ]
-    }
-}
-
-impl FromRgba<u8> for Rg16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([unorm8_to_unorm16(rgba[0]), unorm8_to_unorm16(rgba[1])])
-    }
-}
-
-impl ToRgba<u8> for Rg16Snorm {
-    fn to_rgba(self) -> [u8; 4] {
-        [
-            snorm16_to_unorm8(self.0[0]),
-            snorm16_to_unorm8(self.0[1]),
-            snorm16_to_unorm8(0u16),
-            255u8,
-        ]
-    }
-}
-
-impl FromRgba<u8> for Rg16Snorm {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self([
-            unorm8_to_snorm16(rgba[0]) as u16,
-            unorm8_to_snorm16(rgba[1]) as u16,
-        ])
-    }
-}
-
-impl ToRgba<f32> for Rg16Snorm {
-    fn to_rgba(self) -> [f32; 4] {
-        [
-            snorm16_to_float(self.0[0]),
-            snorm16_to_float(self.0[1]),
-            snorm16_to_float(0u16),
-            1.0,
-        ]
-    }
-}
-
-impl FromRgba<f32> for Rg16Snorm {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self([
-            float_to_snorm16(rgba[0]) as u16,
-            float_to_snorm16(rgba[1]) as u16,
-        ])
-    }
-}
-
-impl ToRgba<u8> for Rgba16 {
-    fn to_rgba(self) -> [u8; 4] {
-        self.0.map(unorm16_to_unorm8)
-    }
-}
-
-impl FromRgba<u8> for Rgba16 {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self(rgba.map(unorm8_to_unorm16))
-    }
-}
-
-impl ToRgba<u8> for Rgba16Snorm {
-    fn to_rgba(self) -> [u8; 4] {
-        self.0.map(snorm16_to_unorm8)
-    }
-}
-
-impl FromRgba<u8> for Rgba16Snorm {
-    fn from_rgba(rgba: [u8; 4]) -> Self {
-        Self(rgba.map(|u| unorm8_to_snorm16(u) as u16))
-    }
-}
-
-impl ToRgba<f32> for Rgba16Snorm {
-    fn to_rgba(self) -> [f32; 4] {
-        self.0.map(snorm16_to_float)
-    }
-}
-
-impl FromRgba<f32> for Rgba16Snorm {
-    fn from_rgba(rgba: [f32; 4]) -> Self {
-        Self(rgba.map(|u| float_to_snorm16(u) as u16))
-    }
-}
-
 pub fn encode_rgba<P, T>(width: u32, height: u32, data: &[T]) -> Result<Vec<u8>, SurfaceError>
 where
     P: FromRgba<T> + Pod,
@@ -644,7 +471,7 @@ where
 
 pub fn decode_rgba<P, T>(width: u32, height: u32, data: &[u8]) -> Result<Vec<T>, SurfaceError>
 where
-    P: Pixel + ToRgba<T>,
+    P: GetPixel + ToRgba<T>,
 {
     validate_length(width, height, std::mem::size_of::<P>(), data)?;
     Ok((0..width * height)
